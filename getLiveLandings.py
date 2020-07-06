d0190346947148a28b663e87117aee8e7f7aaba7
Kevin Walton <kevin@unseen.org>
2020-07-06 18:49:39 +0100

    Updates to improve output and work with Pushover

--- getLiveLandings.py
+++ getLiveLandings.py
@@ -8,11 +8,17 @@ import argparse
 import datetime
 from math import radians, cos, sin, asin, sqrt
 
-def suffix(d):
+# Return "ing: " or "ed: " depending on future or past:
+def landSuffix(t):
+        return 'ing: ' if t>0 else 'ed: '
+
+# Return the correct st/nd/rd etc for a date
+def dateSuffix(d):
     return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
 
+# Allow the use of the correct st/nd/rd etc for a date
 def custom_strftime(format, t):
-    return time.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+    return time.strftime(format).replace('{S}', str(t.day) + dateSuffix(t.day))
 
 # Get distance between two positions in km
 def haversine(lat1, lng1, lat2, lng2):
@@ -53,9 +59,13 @@ argParser.add_argument('distance',
                        help='The max distance from home in km'
                        )
 
-args = argParser.parse_args()
+argParser.add_argument('time',
+                       metavar='t',
+                       type=int,
+                       help='The max Landing time from or till now in seconds'
+                       )
 
-home = {'latitude': args.lat, 'longitude':  args.long}
+args = argParser.parse_args()
 
 # url for manifest containing landing predictions
 ManifestURL = 'https://legacy-snus.habhub.org/tracker/get_predictions.php'
@@ -65,6 +75,7 @@ vehicles = requests.get(ManifestURL).json()
 
 #print(len(vehicles))
 #print(vehicles)
+
 
 # Build list of near-by landings
 output = ""
@@ -80,43 +91,53 @@ for vehicle in vehicles:
     #print(type(locations))
 
     # Get the last predicted location
-    try: 
+    try:
         landing = locations[-1]
     except:
         #print("Error: No locations predicted for this vehicle:", file=sys.stderr)
-        #print(vehicle, file=sys.stderr) 
+        #print(vehicle, file=sys.stderr)
         continue
     #print(landing)
     #print(type(landing))
 
-    landingDist = haversine(home['latitude'], home['longitude'], float(landing['lat']), float(landing['lon']))
-
+    # Distance in km away from 'home' the balloon is predicted to land
+    landingDist = haversine(args.lat, args.long, float(landing['lat']), float(landing['lon']))
     #print("Name: {}, Distance {:2.0f}km".format(vehicle['vehicle'],landingDist))
 
     if landingDist <= args.distance:
-        #print(int(landing['time']) - time.time())
-        landingTimeDiff = abs(int(landing['time']) - time.time())
-        if landingTimeDiff < 86400:
 
-#            landingTimeStr = time.strftime('%a %d %H:%M', time.localtime(int(landing['time'])))
+        # Difference in time between the predicted balloon landing time and now (may be negative or positive)
+        #print(int(landing['time']) - time.time())
+        landingTimeDiff = int(int(landing['time']) - time.time())
+        if abs(landingTimeDiff) < args.time:
+
             landingTime = time.localtime(int(landing['time']))
-            landingTimeStr = (time.strftime('%a %d', landingTime)
-                             + suffix(int(time.strftime('%d', landingTime)))
-                             + " at " 
+            # Create: "ing: Mon 6th at 18:10"
+            landingTimeStr = (landSuffix(landingTimeDiff)
+                             + time.strftime('%a %-d', landingTime)
+                             + dateSuffix(int(time.strftime('%-d', landingTime)))
+                             + " at "
                              + time.strftime('%H:%M', time.localtime(int(landing['time']))))
 
             confidence = vehicle['descending'] + vehicle['landed']
 
-            output += "Name: {}, Confidence {}, Distance {:2.0f}km, Landing: {}, https://www.google.com/maps/dir/{:2.6f},{:2.6f}/{:2.6f},{:2.6f}\n\r".format(
-                       vehicle['vehicle'], 
-                       confidence, 
+            # Build putput string
+            output += "Name: {}, Confidence {}, Distance {:2.0f}km, Land{}, http://sondehub.org/{}, https://www.google.com/maps/dir/{:2.6f},{:2.6f}/{:2.6f},{:2.6f}\n\r".format(
+                       vehicle['vehicle'],
+                       confidence,
                        landingDist,
                        landingTimeStr,
-                       home['latitude'], home['longitude'],
+                       vehicle['vehicle'][3:],
+                       args.lat, args.long,
                        float(landing['lat']), float(landing['lon']))
+
 
 # If any landings were in range output these
 if len(output) > 0:
-    print('Predictions for within {:d}km of home out of {:d} total predictions. \n\r'.format(args.distance, len(vehicles)))
+    print('Predictions for within {:d}km of home within {:d} hours, out of {:d} total predictions. \n\r'.format(
+               args.distance,
+               int(args.time / 60 / 60),
+               len(vehicles)))
     print(output)
+    sys.exit(1)
 
